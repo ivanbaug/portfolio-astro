253cb19603dfa4d779e7e7abf0dc1e6cc8a8f322
ivanbaug <ivanbgp@gmail.com>
2024-10-29 22:17:29 -0500

    feat: add custom class to replicate the container behaviour of bootstrap

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,5 +8,33 @@ export default {
 			  },
 		},
 	},
-	plugins: [],
+	plugins: [
+		function({ addComponents, theme }) {
+			addComponents({
+			  '.bs-container': {
+				width: '100%',
+				paddingRight: 'calc(1.5rem * 0.5)',
+				paddingLeft: 'calc(1.5rem * 0.5)',
+				marginRight: 'auto',
+				marginLeft: 'auto',
+				'@screen sm': {
+				  maxWidth: '540px',
+				},
+				'@screen md': {
+				  maxWidth: '720px',
+				},
+				'@screen lg': {
+				  maxWidth: '960px',
+				},
+			  },
+			  '.bs-container-fluid': {
+				width: '100%',
+				paddingRight: 'calc(1.5rem * 0.5)',
+				paddingLeft: 'calc(1.5rem * 0.5)',
+				marginRight: 'auto',
+				marginLeft: 'auto',
+			  },
+			})
+		  },
+	],
 }
